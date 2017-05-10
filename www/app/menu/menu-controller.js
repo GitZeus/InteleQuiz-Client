31526527eddd3e6f7f2be57c014edab0ca67853a
RA21550465 <RA21550465@T1110324066269.dinfor.uniceub>
2017-05-09 22:25:22 -0300

    Adição de novo componente de calendário

--- www/app/menu/menu-controller.js
+++ www/app/menu/menu-controller.js
@@ -4,8 +4,8 @@
     angular
             .module('intelequiz')
             .controller('menuCtrl', menuCtrl);
-    menuCtrl.$inject = ['DADOS', 'SERVICE', 'CLASSES', '$state'];
-    function menuCtrl(DADOS, SERVICE, CLASSES, $state) {
+    menuCtrl.$inject = ['DADOS', 'SERVICE', 'CLASSES', '$state', '$scope'];
+    function menuCtrl(DADOS, SERVICE, CLASSES, $state, $scope) {
         var menuCtrl = this;
 
         init();
@@ -13,6 +13,29 @@
         function init() {
             SERVICE.ionicMaterialInk();
             menuCtrl.usuarioLogado = DADOS.USUARIO_LOGADO;
+
+            $scope.onezoneDatepicker = {
+                date: new Date(), // MANDATORY                     
+                mondayFirst: false,
+//                months: months,
+//                daysOfTheWeek: daysOfTheWeek,
+//                startDate: startDate,
+//                endDate: endDate,
+                disablePastDays: false,
+                disableSwipe: false,
+                disableWeekend: false,
+//                disableDates: disableDates,
+//                disableDaysOfWeek: disableDaysOfWeek,
+                showDatepicker: false,
+                showTodayButton: true,
+                calendarMode: false,
+                hideCancelButton: false,
+                hideSetButton: false,
+//                highlights: highlights,
+                callback: function (value) {
+                    console.warn(value);
+                }
+            }
         }
     }
 })();
--- www/js/app-config.js
+++ www/js/app-config.js
@@ -1,4 +1,4 @@
-angular.module('intelequiz', ['ionic', 'ionic-material', 'toaster', 'ngAnimate', 'ionic-datepicker'])
+angular.module('intelequiz', ['ionic', 'ionic-material', 'toaster', 'ngAnimate', 'ionic-datepicker','onezone-datepicker'])
         .factory('interceptor', ['$q', '$log', 'toaster', function ($q, $log, toaster) {
                 return {
                     'request': function (config) {
